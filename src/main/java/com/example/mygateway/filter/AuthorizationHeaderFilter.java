36b6e63915a031bd0093a91bda5b17392c0a23f4
sweetoxju@gmail.com <sweetoxju@gmail.com>
2022-09-01 14:50:27 +0900

    auth 추가

---
+++ src/main/java/com/example/mygateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,78 @@
+package com.example.mygateway.filter;
+
+import com.example.mygateway.filter.CustomFilter.Config;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+//@RequiredArgsConstructor
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final Environment environment;
+
+    public AuthorizationHeaderFilter(Environment environment){
+            super(AuthorizationHeaderFilter.Config.class);
+            this.environment = environment;
+        }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(AuthorizationHeaderFilter.Config config) {
+        return ((exchange, chain) -> {
+            //Custom pre filter
+            ServerHttpRequest request = exchange.getRequest();
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "no authorization header", HttpStatus.UNAUTHORIZED);
+            }
+            String authHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authHeader.replace("Bearer", "");
+            if (!isJwtValid(jwt)) {
+                return onError(exchange, "jwt token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean returnValue = false;
+        String subject = null;
+        try {
+            subject = Jwts.parser().setSigningKey(environment.getProperty("token.secret"))
+                .parseClaimsJws(jwt).getBody().getSubject();
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (subject == null || subject.isEmpty()) {
+            returnValue = false;
+        } else {
+            returnValue = true;
+        }
+
+        return returnValue;
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String message, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(message);
+        return response.setComplete();
+    }
+}
