2aa7498b034cc70c3ba6cf7381159a51937f1121
sweet <sweetxoju@gmail.com>
2022-09-02 21:29:54 +0900

    spring cloud config 연결

--- src/main/java/com/example/mygateway/MyGatewayApplication.java
+++ src/main/java/com/example/mygateway/MyGatewayApplication.java
@@ -1,7 +1,10 @@
 package com.example.mygateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyGatewayApplication {
@@ -9,5 +12,8 @@ public class MyGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyGatewayApplication.class, args);
     }
-
+    @Bean
+    public HttpTraceRepository httpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
+    }
 }
